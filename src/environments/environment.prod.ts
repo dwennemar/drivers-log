237c61686abcf2827a832f4478957fafe29ff793
Daniel <dev@dwennemar.de>
2020-02-09 20:58:09 +0100

    adding firebase-config to prod-env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDS7R3qUQCXhnDmpmaZ_SncbeCfzgaHR0Q',
+    authDomain: 'dw-driver-log.firebaseapp.com',
+    databaseURL: 'https://dw-driver-log.firebaseio.com',
+    projectId: 'dw-driver-log',
+    storageBucket: 'dw-driver-log.appspot.com',
+    messagingSenderId: '1041400438756',
+    appId: '1:1041400438756:web:f509c7589ca8b34710f1f1',
+    measurementId: 'G-X3YZ5CK0MB'
+  }
 };
